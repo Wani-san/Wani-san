29cd139c0c31da70d33fc3ca7fb1d098206cc594
Wani-san <kamijukkoku_juh@yamanashi-ken.ac.jp>
2023-01-16 17:10:07 +0900

    カリキュラム8−1

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Model\Posts;
+use App\Models\Post;
+
 class PostController extends Controller
 {
-    public function index(Post $post)
-{
-    return $post->get();
+   public function index(Post $post)
+  { 
+    return view('posts/show')->with(['post' => $post]);
+  }
 }
-}
+
+   
+    
+
